2ee7a2ad743ba9474c830ee2a72129527a22502f
Lluís <lluisgh28@gmail.com>
2013-11-22 13:28:19 +0100

    Added put sensor

--- web.rb
+++ web.rb
@@ -1,5 +1,47 @@
-require 'sinatra'
+# Require the bundler gem and then call Bundler.require to load in all gems
+# listed in Gemfile.
+require 'bundler'
+Bundler.require
+
+# Setup DataMapper with a database URL. On Heroku, ENV['DATABASE_URL'] will be
+# set, when working locally this line will fall back to using SQLite in the
+# current directory.
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite")
+
+# Define a simple DataMapper model.
+class Sensor
+  include DataMapper::Resource
+
+  property :id, Serial, :key => true
+  property :created_at, DateTime
+  property :beacon, String, :length => 255
+  property :temperature, Float
+end
+
+# Finalize the DataMapper models.
+DataMapper.finalize
+
+# Tell DataMapper to update the database according to the definitions above.
+DataMapper.auto_upgrade!
+
+
+put '/sensor/:beacon:temperature' do
+	content_type :json
+	
+	@sensor = Sensor.first_or_new(:beacon => params[:beacon])
+	@sensor.temperature = params[:temperature]
+
+	if @sensor.save
+    	@sensor.to_json
+ 	else
+    	halt 404
+  	end
+end
 
 get '/' do
   "Hello, world\n"
+end
+
+put '/location/:device/:beacon' do
+
 end
